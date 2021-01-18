a8e4d53b16000cda9c029aac5735ec1036487409
Rafael_Rache <rafaelmarquesrache@gmail.com>
2021-01-18 20:46:09 -0300

    Update Instructions

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -8,12 +8,16 @@ function Instructions() {
                     <br/>cd "folder"
                     <br/>ls
                     <br/>fileName.exe
+                    <br/>touch "file"
+                    <br/>mkdir "folder"
                 </div>
                 <div>
                     move up one directory 
                     <br/>enter "folder"
                     <br/>list all files in folder
                     <br/>runs the file
+                    <br/>create a file
+                    <br/>create a folder
                 </div>
             </div>
         </div>
